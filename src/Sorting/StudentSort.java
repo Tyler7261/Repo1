2e00c4503c161b61d2c24b58ce06cdcb06cd1709
tyle7261 <tyle7261@ISS2V1SKH2.scdsb.pri>
2017-11-24 10:29:50 -0500

    started next assignment

--- src/Sorting/StudentSort.java
+++ src/Sorting/StudentSort.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//modified to work with ISSStudent 
 package Sorting;
 
 import java.io.BufferedReader;
